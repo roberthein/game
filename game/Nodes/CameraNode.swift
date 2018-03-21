7c8f783d32a80c9709bca1a68444702df94dbac2
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2018-03-21 15:44:28 +0100

    Add camera effects.

--- game/Nodes/CameraNode.swift
+++ game/Nodes/CameraNode.swift
@@ -4,16 +4,23 @@ import SceneKit
 class CameraNode: SCNNode {
     
     let distance: Float = 5
-    let height: Float = 3
+    let height: Float = 10
     
     private lazy var scnCamera: SCNCamera = {
-        return SCNCamera()
+        let camera = SCNCamera()
+        camera.bloomIntensity = 1.5
+        camera.bloomBlurRadius = 5
+        camera.bloomThreshold = 0.2
+        camera.wantsHDR = true
+        camera.wantsExposureAdaptation = false
+        
+        return camera
     }()
     
     private lazy var container: SCNNode = {
         let node = SCNNode()
         node.camera = scnCamera
-        node.transform = SCNMatrix4Mult(SCNMatrix4MakeRotation(-Float.pi / 8, 1, 0, 0), SCNMatrix4MakeRotation(-Float.pi / 4, 0, 1, 0))
+        node.transform = SCNMatrix4Mult(SCNMatrix4MakeRotation(-Float.pi / 4, 1, 0, 0), SCNMatrix4MakeRotation(-Float.pi / 4, 0, 1, 0))
         
         return node
     }()
